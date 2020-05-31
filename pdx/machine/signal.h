e3a5bb058d4a6cb8b45cf5e038f7706718291859
adamy <adamy@git.code.sf.net>
2020-05-31 22:04:51 +0100

    prototypes

---
+++ pdx/machine/signal.h
@@ -0,0 +1,26 @@
+/*
+ */
+#ifndef  MACHINE_SIGNAL_H_INCLUDED
+#define  MACHINE_SIGNAL_H_INCLUDED
+
+#include <signal.h>
+
+#if defined(WIN32)
+#if defined(__WATCOMC__)
+#define  SIGTRAP        (_SIGMAX+1)
+#else
+#define  SIGTRAP        (NSIG+1)
+#endif
+
+#elif defined(MSDOS)
+#if defined(__WATCOMC__)
+#define  SIGTRAP        (_SIGMAX+1)
+#else
+#define  SIGTRAP        (NSIG+1)
+#endif
+
+#endif
+
+#endif   /*MACHINE_SIGNAL_H_INCLUDED*/
+
+
