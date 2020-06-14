9636411b672af7d33959c460ae7274989fe3648d
adamy <adamy@git.code.sf.net>
2020-06-14 13:59:51 +0100

    msvc 19.6

--- contrib/include/sys/utypes.h
+++ contrib/include/sys/utypes.h
@@ -1,7 +1,7 @@
 #ifndef LIBW32_SYS_UTYPES_H_INCLUDED
 #define LIBW32_SYS_UTYPES_H_INCLUDED
 //#include <edidentifier.h>
-//__CIDENT_RCSID(gr_libw32_sys_utypes_h,"$Id: utypes.h,v 1.2 2020/05/31 20:24:59 cvsuser Exp $")
+//__CIDENT_RCSID(gr_libw32_sys_utypes_h,"$Id: utypes.h,v 1.3 2020/06/13 23:30:13 cvsuser Exp $")
 //__CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*- */
@@ -20,8 +20,8 @@
 #if (_MSC_VER != 1600)                          /* MSVC 10/2010 */
 #if (_MSC_VER != 1900)                          /* MSVC 19/2015 */
 #if (_MSC_VER <  1910 || _MSC_VER > 1916)       /* MSVC 19.10 .. 16/2017 */
-#if (_MSC_VER > 1920)                           /* MSVC 19.20 /2019 */
-#error utypes.h: untested MSVC Version (2005 -- 2017) only ...
+#if (_MSC_VER > 1926)                           /* MSVC 19.20 /2019 */
+#error utypes.h: untested MSVC Version (2005 -- 2019.06) only ...
  //see: https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B
 #endif //2019
 #endif //2017
@@ -127,7 +127,9 @@ typedef int pid_t;                              /* process identifier */
 
 typedef long suseconds_t;                       /* sys/types.h */
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && \
+	!defined(__WATCOMC__)
+		/* check for !WATCOMC, at times we masquerade WC as MSVC */
 #if !defined(uid_t) && !defined(gid_t)
 typedef int uid_t;
 typedef int gid_t;
